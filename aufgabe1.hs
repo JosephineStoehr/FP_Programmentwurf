43720db91b55bffe7353f2762e3745a2eb8f041e
Clemens Szostack <clemenssz@gmx.de>
2023-05-18 09:18:32 +0200

    Initial implementation

--- aufgabe1.hs
+++ aufgabe1.hs
@@ -1,70 +1,72 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use head" #-}
-data Schule = Schule
+data School = School
     {
-        nameSchule :: [Char],
-        plaetze :: Int
+        nameSchool :: String,
+        spots :: Int
     } deriving (Show, Eq)
 
-data Schueler = Schueler
+data Student = Student
     {
-        nameSchueler :: [Char],
-        note :: Float,
-        praeferenz1 :: Schule,
-        praeferenz2 :: Schule,
-        praeferenz3 :: Schule
+        nameStudent :: String,
+        mark :: Float,
+        preferences:: [String]
     } deriving Show
 
-data Besetzung = Besetzung
+data Assignment = Assignment
     {
-        schule :: Schule,
-        liste :: [Schueler]
-
+        assignmentSchoolName :: String,
+        assignedStudents :: [String]
     } deriving Show
 
-schuelerNachNotenSortieren :: [Schueler] -> [Schueler]
-schuelerNachNotenSortieren [] = []
-schuelerNachNotenSortieren (x:xs) = schuelerNachNotenSortieren smaller  ++ [x] ++ schuelerNachNotenSortieren larger where
-    smaller = [s| s <- xs , note s <= note x]
-    larger = [l | l<- xs , note l > note x]
+sortStudentsByMarks :: [Student] -> [Student]
+sortStudentsByMarks [] = []
+sortStudentsByMarks (x:xs) = sortStudentsByMarks smaller  ++ [x] ++ sortStudentsByMarks larger where
+    smaller = [s| s <- xs , mark s <= mark x]
+    larger = [l | l<- xs , mark l > mark x]
 
-schuelerNachPraeferenzenFiltern :: Schule -> [Schueler] -> Int -> [Schueler]
-schuelerNachPraeferenzenFiltern _ [] _ = []
-schuelerNachPraeferenzenFiltern schule liste p = preaf1 schule liste ++ preaf2 schule liste ++ preaf3 schule liste
-    where 
-        preaf1 _ [] = []
-        preaf1 schule (x:xs)
-            | praeferenz1 x == schule = x : preaf1 schule xs
-            | otherwise = preaf1 schule xs
-        preaf2 _ [] = []
-        preaf2 schule (x:xs)
-            | praeferenz2 x == schule = x : preaf2 schule xs
-            | otherwise = preaf2 schule xs
-        preaf3 _ [] = []
-        preaf3 schule (x:xs)
-            | praeferenz3 x == schule = x : preaf3 schule xs
-            | otherwise = preaf3 schule xs
+generateCompleteAssignmentTable :: [School] -> [Student] -> [Int] -> [Assignment]-> [Assignment]
+generateCompleteAssignmentTable _ _ [] currentAssignment = currentAssignment
+generateCompleteAssignmentTable schools students (x:xs) currentAssignment = generateCompleteAssignmentTable schools students xs (generateAssignmentsForAllSchoolsOnPreference schools students x currentAssignment)
 
+generateAssignmentsForAllSchoolsOnPreference :: [School] -> [Student] -> Int -> [Assignment] -> [Assignment]
+generateAssignmentsForAllSchoolsOnPreference schools students currentPreference currentAssignment = [generateAssignmentForSpecificSchoolAndPreference x (determineUnassignedStudents students currentAssignment) currentPreference (findAssignmentBelongingToSchool x currentAssignment)| x <- schools]
+
+generateAssignmentForSpecificSchoolAndPreference :: School -> [Student] -> Int -> Assignment -> Assignment
+generateAssignmentForSpecificSchoolAndPreference _ [] _ assignemnt = assignemnt
+generateAssignmentForSpecificSchoolAndPreference school (x:xs) preference assignemnt =
+    if doesStudentHavePreference school x preference && not (isSchoolFull school assignemnt)
+        then generateAssignmentForSpecificSchoolAndPreference school xs preference assignemnt {assignedStudents = assignedStudents assignemnt ++ [nameStudent x]}
+        else generateAssignmentForSpecificSchoolAndPreference school xs preference assignemnt
+
+findAssignmentBelongingToSchool :: School -> [Assignment] -> Assignment
+findAssignmentBelongingToSchool school [] = Assignment {assignmentSchoolName = nameSchool school, assignedStudents = []}
+findAssignmentBelongingToSchool school (x:xs) =
+    if nameSchool school == assignmentSchoolName x
+        then x
+        else findAssignmentBelongingToSchool school xs
+
+determineUnassignedStudents :: [Student] -> [Assignment] -> [Student]
+determineUnassignedStudents students assignment = [x | x <- students, nameStudent x `notElem` generateListOfAlreadyAssignedStudents assignment]
+
+generateListOfAlreadyAssignedStudents :: [Assignment] -> [String]
+generateListOfAlreadyAssignedStudents = concatMap assignedStudents
+
+isSchoolFull :: School -> Assignment -> Bool
+isSchoolFull school assignment = length (assignedStudents assignment) > spots school - 1
+
+doesStudentHavePreference :: School -> Student -> Int ->  Bool
+doesStudentHavePreference school students currentPreference = nameSchool school == preferences students!!currentPreference
+
+
+main :: IO ()
 main = do
-    let schulListe = [(Schule {nameSchule = "HWR", plaetze = 5 }),(Schule {nameSchule = "HTW", plaetze = 4 }),(Schule {nameSchule = "TH Wildau", plaetze = 2 }),(Schule {nameSchule = "xyz", plaetze = 1 })]
-    --print schulListe --[Schule {nameSchule = "HWR", plaetze = 5},Schule {nameSchule = "HTW", plaetze = 4},Schule {nameSchule = "TH Wildau", plaetze = 3}]
-    let schuelerListe = [
-                        (Schueler {nameSchueler="Emilie", note=1.0, praeferenz1=schulListe!!0,praeferenz2=schulListe!!2,praeferenz3 =schulListe!!1}),
-                        (Schueler {nameSchueler="Clemens", note=1.0, praeferenz1=schulListe!!1,praeferenz2=schulListe!!2,praeferenz3 =schulListe!!0}),
-                        (Schueler {nameSchueler="Josephine", note=1.0, praeferenz1=schulListe!!0,praeferenz2=schulListe!!3,praeferenz3 =schulListe!!1}),
+    let schulListe = [(School {nameSchool = "HWR", spots = 2}),(School {nameSchool = "HTW", spots = 3 }),(School {nameSchool = "TH Wildau", spots = 10 })]
+    let schuelerListe = [(Student {nameStudent="Emilie", mark=1.0, preferences = ["HWR", "HTW", "TH Wildau"]}),(Student {nameStudent="Clemens", mark=1.0, preferences = ["HTW", "HWR", "TH Wildau"]}),(Student {nameStudent="Josephine", mark=1.0, preferences = ["TH Wildau", "HWR", "HTW"]}),(Student {nameStudent="Tom Vorlost", mark=1.7, preferences = ["HWR", "HTW", "TH Wildau"]}),(Student {nameStudent="Hermine", mark=1.0, preferences =["TH Wildau", "HTW", "HWR"]}),(Student {nameStudent="Ron", mark=3.7, preferences =["HTW", "HWR", "TH Wildau"]}),(Student {nameStudent="Harry", mark=2.3, preferences =["HWR", "TH Wildau", "HTW"]}),(Student {nameStudent="Malfoy", mark=2.3, preferences =["HTW", "HWR", "TH Wildau"]}),(Student {nameStudent="Hirka", mark=2.7, preferences =["TH Wildau", "HWR", "HTW"]}),(Student {nameStudent="Rime", mark=1.3, preferences =["HTW", "HWR", "TH Wildau"]}),(Student {nameStudent="Eragon", mark=4.0, preferences =["TH Wildau", "HWR", "HTW"]}),(Student {nameStudent="Ezio Auditore", mark=3.0, preferences =["HWR", "HTW", "TH Wildau"]})]
 
-                        (Schueler {nameSchueler="Tom Vorlost", note=1.7, praeferenz1=schulListe!!3,praeferenz2=schulListe!!1,praeferenz3 =schulListe!!2}),
-                        (Schueler {nameSchueler="Hermine", note=1.0, praeferenz1=schulListe!!2,praeferenz2=schulListe!!1,praeferenz3 =schulListe!!0}),
-                        (Schueler {nameSchueler="Ron", note=3.7, praeferenz1=schulListe!!2,praeferenz2=schulListe!!0,praeferenz3 =schulListe!!1}),
-
-                        (Schueler {nameSchueler="Harry", note=2.3, praeferenz1=schulListe!!0,praeferenz2=schulListe!!2,praeferenz3 =schulListe!!1}),
-                        (Schueler {nameSchueler="Malfoy", note=2.3, praeferenz1=schulListe!!2,praeferenz2=schulListe!!1,praeferenz3 =schulListe!!0}),
-                        (Schueler {nameSchueler="Hirka", note=2.7, praeferenz1=schulListe!!0,praeferenz2=schulListe!!1,praeferenz3 =schulListe!!2}),
-
-                        (Schueler {nameSchueler="Rime", note=1.3, praeferenz1=schulListe!!0,praeferenz2=schulListe!!2,praeferenz3 =schulListe!!1}),
-                        (Schueler {nameSchueler="Eragon", note=4.0, praeferenz1=schulListe!!1,praeferenz2=schulListe!!2,praeferenz3 =schulListe!!0}),
-                        (Schueler {nameSchueler="Ezio Auditore", note=3.0, praeferenz1=schulListe!!1,praeferenz2=schulListe!!0,praeferenz3 =schulListe!!2})
-                        ]
-    --print schuelerListe --[Schueler {nameSchueler = "Emilie", note = 1.0, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HTW", plaetze = 4}},Schueler {nameSchueler = "Clemens", note = 1.0, praeferenz1 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HWR", plaetze = 5}},Schueler {nameSchueler = "Josephine", note = 1.0, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HTW", plaetze = 4}},Schueler {nameSchueler = "Tom Vorlost", note = 1.7, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz3 = Schule {nameSchule = "TH Wildau", plaetze = 3}},Schueler {nameSchueler = "Hermine", note = 1.0, praeferenz1 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz2 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz3 = Schule {nameSchule = "HWR", plaetze = 5}},Schueler {nameSchueler = "Ron", note = 3.7, praeferenz1 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz2 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz3 = Schule {nameSchule = "HTW", plaetze = 4}},Schueler {nameSchueler = "Harry", note = 2.3, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HTW", plaetze = 4}},Schueler {nameSchueler = "Malfoy", note = 2.3, praeferenz1 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz2 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz3 = Schule {nameSchule = "HWR", plaetze = 5}},Schueler {nameSchueler = "Hirka", note = 2.7, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz3 = Schule {nameSchule = "TH Wildau", plaetze = 3}},Schueler {nameSchueler = "Rime", note = 1.3, praeferenz1 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HTW", plaetze = 4}},Schueler {nameSchueler = "Eragon", note = 4.0, praeferenz1 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz2 = Schule {nameSchule = "TH Wildau", plaetze = 3}, praeferenz3 = Schule {nameSchule = "HWR", plaetze = 5}},Schueler {nameSchueler = "Ezio Auditore", note = 3.0, praeferenz1 = Schule {nameSchule = "HTW", plaetze = 4}, praeferenz2 = Schule {nameSchule = "HWR", plaetze = 5}, praeferenz3 = Schule {nameSchule = "TH Wildau", plaetze = 3}}]
-    --print (schuelerNachNotenSortieren schuelerListe)
-    print (schuelerNachPraeferenzenFiltern (schulListe!!3) schuelerListe 1)
+    --let besetzung1 = generateAssignmentsForAllSchoolsOnPreference schulListe schuelerListe 0 []
+    --let besetzung2 = generateAssignmentsForAllSchoolsOnPreference schulListe schuelerListe 1 besetzung1
+    --let besetzung3 = generateAssignmentsForAllSchoolsOnPreference schulListe schuelerListe 2 besetzung2
+    --print besetzung1
+    --print besetzung2
+    --print besetzung3
+    print $ sortStudentsByMarks schuelerListe
+    print $ generateCompleteAssignmentTable schulListe schuelerListe [0,1,2] []
